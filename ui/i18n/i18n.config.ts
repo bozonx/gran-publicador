d9b8e238abb7e33a8f0fbc9b921812c8e235c4f6
Ivan <ipkozyrin@gmail.com>
2026-01-08 20:54:56 -0300

    fix

---
+++ ui/i18n/i18n.config.ts
@@ -0,0 +1,53 @@
+export default () => {
+  return {
+    legacy: false,
+    locale: 'en-US',
+    datetimeFormats: {
+      'en-US': {
+        short: {
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric'
+        },
+        long: {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          weekday: 'long',
+          hour: 'numeric',
+          minute: 'numeric'
+        }
+      },
+      'ru-RU': {
+        short: {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric'
+        },
+        long: {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          weekday: 'long',
+          hour: 'numeric',
+          minute: 'numeric'
+        }
+      },
+      'ru': {
+        short: {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric'
+        },
+        long: {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          weekday: 'long',
+          hour: 'numeric',
+          minute: 'numeric'
+        }
+      }
+    }
+  }
+}
