82c67f0d7bc6a2e0b3ef3518ba38ba380f52d06a
Ivan <ipkozyrin@gmail.com>
2026-01-12 22:19:00 -0300

    test

--- test/unit/debug.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from '@jest/globals';
-import nock from 'nock';
-
-describe('Debug Test', () => {
-  it('should run correctly in ESM mode', () => {
-    expect(nock).toBeDefined();
-    expect(true).toBe(true);
-  });
-});
