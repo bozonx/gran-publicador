3acece4a33eacaf97279ce81dd9b1cd9d24539f9
Ivan <ipkozyrin@gmail.com>
2026-01-22 11:38:18 -0300

    fix

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service.js';
 import { RolesController } from './roles.controller.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
+import { ApiTokensModule } from '../api-tokens/api-tokens.module.js';
+import { JwtOrApiTokenGuard } from '../../common/guards/jwt-or-api-token.guard.js';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ApiTokensModule],
   controllers: [RolesController],
-  providers: [RolesService],
+  providers: [RolesService, JwtOrApiTokenGuard],
   exports: [RolesService],
 })
 export class RolesModule {}
