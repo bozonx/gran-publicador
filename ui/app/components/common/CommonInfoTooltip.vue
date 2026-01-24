6038f531abf18688c2c8d8ead454e690c60bd10f
Ivan <ipkozyrin@gmail.com>
2026-01-24 20:27:22 -0300

    fix

--- ui/app/components/common/CommonInfoTooltip.vue
+++ ui/app/components/common/CommonInfoTooltip.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 interface Props {
   text: string
 }
 
 defineProps<Props>()
+
+const open = ref(false)
 </script>
 
 <template>
-  <UTooltip :text="text">
-    <UIcon name="i-heroicons-information-circle" class="w-4 h-4 text-gray-400 hover:text-gray-500 cursor-help" />
+  <UTooltip :text="text" v-model:open="open" :popper="{ placement: 'top' }">
+    <UIcon
+      name="i-heroicons-information-circle"
+      class="w-4 h-4 text-gray-400 hover:text-gray-500 cursor-help"
+      @click="open = true"
+      @mouseenter="open = true"
+      @mouseleave="open = false"
+    />
   </UTooltip>
 </template>
