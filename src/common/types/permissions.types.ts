f90d0436af85717c0590d15f87978323183d61c2
Ivan <ipkozyrin@gmail.com>
2026-02-11 18:16:10 -0300

    fix

--- src/common/types/permissions.types.ts
+++ src/common/types/permissions.types.ts
@@ -1,7 +1,2 @@
+export type { RolePermissions } from '@gran/shared/permissions';
 export { PermissionKey, SystemRoleType } from '@gran/shared/permissions';
-
-export type RolePermissions = import('@gran/shared/permissions').RolePermissions;
-
-export type SystemRoleType = import('@gran/shared/permissions').SystemRoleType;
-
-export type PermissionKey = import('@gran/shared/permissions').PermissionKey;
