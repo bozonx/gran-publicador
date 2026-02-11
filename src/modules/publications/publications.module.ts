a3781e7c7b7b679494504a2d07608c8391dadc14
Ivan <ipkozyrin@gmail.com>
2026-02-11 09:23:22 -0300

    fix

--- src/modules/publications/publications.module.ts
+++ src/modules/publications/publications.module.ts
@@ -4,11 +4,12 @@ import { PrismaModule } from '../prisma/prisma.module.js';
 import { ApiTokensModule } from '../api-tokens/api-tokens.module.js';
 import { SocialPostingModule } from '../social-posting/social-posting.module.js';
 import { LlmModule } from '../llm/llm.module.js';
+import { TagsModule } from '../tags/tags.module.js';
 import { PublicationsController } from './publications.controller.js';
 import { PublicationsService } from './publications.service.js';
 
 @Module({
-  imports: [PrismaModule, ApiTokensModule, SocialPostingModule, LlmModule],
+  imports: [PrismaModule, ApiTokensModule, SocialPostingModule, LlmModule, TagsModule],
   controllers: [PublicationsController],
   providers: [PublicationsService],
   exports: [PublicationsService],
--- src/modules/tags/tags.module.ts
+++ src/modules/tags/tags.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TagsService } from './tags.service.js';
 import { TagsController } from './tags.controller.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
+import { ApiTokensModule } from '../api-tokens/api-tokens.module.js';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ApiTokensModule],
   controllers: [TagsController],
   providers: [TagsService],
   exports: [TagsService],
